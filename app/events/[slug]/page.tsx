58db23476ab7ad35861d787ec220c088a58ad8d6
nishchayMish <nishchaymishra2100@gmail.com>
2026-03-15 12:55:57 +0530

    feat: Add a detailed skeleton loading state for event details and include email in event booking tracking.

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from "react";
 import EventDetails from "@/components/EventDetails"
+import EventDetailsSkeleton from "@/components/EventDetailsSkeleton";
 
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
     const slug = params.then((p) => p.slug);
 
     return (
         <main>
-            <Suspense fallback={<div>Loading...</div>}>
+            <Suspense fallback={<EventDetailsSkeleton />}>
                 <EventDetails params={slug} />
             </Suspense>
         </main>
